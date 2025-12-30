22a0f82f2250bb99cac296c3a6382e61178ad906
rcamargopires-cmd <r.camargo.pires@gmail.com>
2025-12-30 16:01:20 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Falha ao encontrar o elemento root');
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
