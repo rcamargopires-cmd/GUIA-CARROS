4053664d1016a256ec91b99661e44e1f68813bbc
rcamargopires-cmd <r.camargo.pires@gmail.com>
2025-12-30 15:53:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
+  root: './', // Garante que ele olhe na pasta principal
+  base: './', // Ajuda a Vercel a encontrar os caminhos dos arquivos
   build: {
     outDir: 'dist',
   }
